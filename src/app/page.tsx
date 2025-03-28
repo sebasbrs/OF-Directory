3cd9e9379123e6a15e610dd0ba05f6896023058f
sebasbrs <sebasgodoy32@gmail.com>
2025-03-28 12:16:44 -0500

    Base Framework

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">¡TailwindCSS está funcionando! 🚀</h1>
+    </div>
+  );
+}
