9e40da2bf068ccd51171d77e594f6f2b0ef2390b
sebasbrs <sebasgodoy32@gmail.com>
2025-03-30 10:39:57 -0500

    Fix Children issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import "@/styles/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-export default function RootLayout({ children }) {
+import { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="es">
       <body className="bg-gray-100 text-gray-900">
