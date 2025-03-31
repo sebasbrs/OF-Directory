24a34af2fb511dcce5b0b8c44f204e0bd225e155
sebasbrs <sebasgodoy32@gmail.com>
2025-03-31 09:51:22 -0500

    fix ads issues

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
       <footer className="bg-gray-900 text-white p-4 text-center mt-10">
         <p>© 2025 OnlyFans Directory. No afiliado a OnlyFans.</p>
         <script type="text/javascript" src="https://js.juicyads.com/jp.php?c=44542313t214u4r2o2c4z2d484&u=http%3A%2F%2Fwww.juicyads.rocks"></script>
-<ins id="1086245" data-width="728" data-height="90"></ins>
+          <ins id="1086245" data-width="728" data-height="90"></ins>
       </footer>
     );
   }
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState,useEffect } from "react";
+import { useState } from "react";
 import { getTopModels } from "@/lib/api";
 import ModelCard from "./ModelCard";
 
