74f5652894198b6c5a7eae75c2b764bed4882cf6
sebasbrs <sebasgodoy32@gmail.com>
2025-04-03 11:50:39 -0500

    add dynamic to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import HomePage from "@/components/HomePage";
 import { getTopModels } from "@/lib/api";
 
+export const dynamic = "force-dynamic"; 
+
 export default async function Home() {
   const models = await getTopModels(1);
   return <HomePage initialModels={models.items} />;
